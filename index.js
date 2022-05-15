ac8f8891f70cb3171776845d4df3c7b6109b4cea
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-15 23:33:41 +0600

    code added

--- index.js
+++ index.js
@@ -17,7 +17,45 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        console.log('Database connected')
+        const serviceCollection =  client.db('doctors-portal').collection('services');
+        const bookingCollection =  client.db('doctors-portal').collection('bookings');
+
+        app.get('/service', async(req, res) =>{
+          const query = {}
+          const cursor = serviceCollection.find(query);
+          const services =await cursor.toArray();
+          res.send(services);
+        });
+
+        app.get('/available', async(req, res) =>{
+          const date = req.query.date;
+          const services = await serviceCollection.find().toArray();
+          const query = {date};
+          const booking = await bookingCollection.find(query).toArray();
+          services.forEach(service =>{
+            const serviceBookings = booking.filter(book => book.treatment === service.name);
+            const bookedSlots = serviceBookings.map(book=> book.slot);
+            const available = service.slots.filter(slot=>!bookedSlots.includes(slot));
+            service.slots = available;
+            // service.booked = serviceBookings.map(s > s.slot);
+          })
+          res.send(services);
+
+        })
+
+        app.post('/booking', async(req, res) =>{
+          const booking = req.body;
+          const query ={
+            treatment: booking.treatment, data: booking.data, patient: booking.patient
+          }
+          const exists = await bookingCollection.findOne(query);
+          if(exists){
+            return res.send({success: false, booking: exists})
+          }
+          const result = await bookingCollection.insertOne(booking);
+          return res.send({success: true, result});
+
+        })
     }
     finally{
 
